307f3f3fcd39a22391c2518124400e8bc19a52f2
Suraphan(Rawd) Laokondee <suraphan.laokondee@scb.co.th>
2019-09-13 14:21:49 +0700

    feat: Added Home and Login Module

---
+++ Login/Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Suraphan Laokondee on 13/9/19.
+//  Copyright © 2019 SCB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
