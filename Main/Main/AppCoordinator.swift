17369118b564cadc2287133b927989939fa40113
Suraphan(Rawd) Laokondee <suraphan.laokondee@scb.co.th>
2019-09-13 14:33:22 +0700

    feat: added CoordinatorKit and remove wrong Home, Login  module

---
+++ Main/Main/AppCoordinator.swift
@@ -0,0 +1,17 @@
+//
+//  AppCoordinator.swift
+//  Main
+//
+//  Created by Suraphan Laokondee on 13/9/19.
+//  Copyright © 2019 SCB. All rights reserved.
+//
+
+import Foundation
+import CoordinatorKit
+
+// The coordinator and its children create a composite pattern
+// From here it would be easy to make an iterator of the children
+// Or use chain of responsibility to bubble up errors
+class AppCoordinator {
+  
+}
